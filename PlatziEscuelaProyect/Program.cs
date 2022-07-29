a0c593201bc6728614f0d5941c7d0df7f7d8d3cd
SekaitsuSistecredito <lbernategordillo@gmail.com>
2022-07-29 09:50:05 -0500

    Ejercicio en platzi donde se hace busqueda y eliminación de objetos por colecciones  y predicate

--- PlatziEscuelaProyect/Program.cs
+++ PlatziEscuelaProyect/Program.cs
@@ -16,22 +16,13 @@ namespace Etapa1
             //LISTAS
             escuela.Cursos = new List<Curso>()
             {
-                new Curso(){ Nombre = "101", Jornada = TiposJornada.Mañana },
+                new Curso() { Nombre = "101", Jornada = TiposJornada.Mañana },
                 new Curso() {Nombre = "201", Jornada = TiposJornada.Mañana},
-                new Curso{Nombre = "301", Jornada = TiposJornada.Mañana}
+                new Curso   {Nombre = "301", Jornada = TiposJornada.Mañana}
             };
+
             escuela.Cursos.Add(new Curso { Nombre = "402", Jornada = TiposJornada.Tarde });
             escuela.Cursos.Add(new Curso { Nombre = "502", Jornada = TiposJornada.Tarde });
-
-            //ARREGLO
-            //escuela.Cursos = new Curso[]
-            //{
-            //    new Curso(){ Nombre = "101", Jornada = TiposJornada.Mañana },
-            //    new Curso() {Nombre = "201", Jornada = TiposJornada.Mañana},
-            //    new Curso{Nombre = "301", Jornada = TiposJornada.Mañana}
-            //};
-
-
 
             var otrColeccion = new List<Curso>(){
                         new Curso(){ Nombre = "601", Jornada = TiposJornada.Mañana },
@@ -39,21 +30,24 @@ namespace Etapa1
                         new Curso{Nombre = "801", Jornada = TiposJornada.Tarde}
             };
 
+            //Curso tmp = new Curso { Nombre = "101 Vacacional", Jornada = TiposJornada.Noche };
+            //escuela.Cursos.Add(tmp);
+            //WriteLine("Curso.Hash " + tmp.GetHashCode());
+
             escuela.Cursos.AddRange(otrColeccion);
             ImpimirCursosEscuela(escuela);
 
-            otrColeccion.Clear();
+            Predicate<Curso> miAlgoritmo = Predicado;
+            escuela.Cursos.RemoveAll(Predicado);
 
-            //escuela.Cursos.RemoveAll(delegate (Curso cur)
-            //                        {
-            //                            return cur.Nombre == "301";
-            //                        });
-
-            //escuela.Cursos.RemoveAll((cur)=> cur.Nombre == "501" && cur.Jornada == TiposJornada.Mañana );
-            
-            WriteLine("==================");
+            WriteLine("=======------------======");
             ImpimirCursosEscuela(escuela);
+            //otrColeccion.Clear();
+        }
 
+        private static bool Predicado(Curso curobj)
+        {
+            return curobj.Nombre == "301";
         }
 
         private static void ImpimirCursosEscuela(Escuela escuela) //FUNCIONA POR QUE  USA escuela.Cursos
@@ -109,107 +103,3 @@ namespace Etapa1
         }
     }
 }
-
-
-//using System;
-//using static System.Console; //se añade solo para escribier WriteLine al imprimir
-//using PlatziEscuelaProyect.Entidades;// se llaman los otros archivos desde el namespace a este archivo
-//using System.Collections.Generic;
-
-//namespace PlatziEscuelaProyect
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            var escuela = new Escuela ("Platzi Academy", 2012, TiposEscuela.Primaria,
-//                        ciudad: "Bogotá", pais: "Colombia"
-//                        );
-
-//            List<Curso> escuelaCursos = new List<Curso>()
-//            {
-//                new Curso() { Name = "101", Jornada = TiposJornada.Mañana },
-//                new Curso() { Name = "201", Jornada = TiposJornada.Mañana },
-//                new Curso { Name = "301", Jornada = TiposJornada.Mañana },
-//                new Curso { Name = "102", Jornada = TiposJornada.Tarde },
-//                new Curso { Name = "202", Jornada = TiposJornada.Tarde }
-//            };
-
-//            List<Curso> otroColeccion = new List<Curso>(){
-//                        new Curso(){ Name = "401", Jornada = TiposJornada.Mañana },
-//                        new Curso() {Name = "501", Jornada = TiposJornada.Mañana},
-//                        new Curso{Name = "501", Jornada = TiposJornada.Tarde}
-//            };
-
-//            escuelaCursos.Add(new Curso() {Name =" 601", Jornada= TiposJornada.Mañana});
-
-//            WriteLine("==================");
-//            ImpimirCursosEscuela(escuela);
-//            /*
-
-
-//            escuela.Cursos.RemoveAll(delegate (Curso cur)
-//                                    {
-//                                        return cur.Nombre == "301";
-//                                    });
-
-//            escuela.Cursos.RemoveAll((cur)=> cur.Nombre == "501" && cur.Jornada == TiposJornada.Mañana );
-//            */
-
-
-
-//        }
-
-//        private static void ImpimirCursosEscuela(Escuela escuela)
-//        {
-//            WriteLine("====================");
-//            WriteLine("Cursos de la Escuela");
-//            WriteLine("====================");
-
-//            if (escuela.Cursos != null)
-//            {
-//                foreach (var curso in escuela.Cursos)
-//                {
-//                    WriteLine($"Nombre {curso.Name}, Id  {curso.UniqueId}");
-//                }
-//            }
-//        }
-
-
-//        private static void ImprimirCursosWhile(Curso[] arregloCursos)
-//        {
-//            int contador = 0;
-//            while (contador < arregloCursos.Length)
-//            {
-//                Console.WriteLine($"Nombre {arregloCursos[contador].Name}, Id  {arregloCursos[contador].UniqueId}");
-//                contador++;
-//            }
-//        }
-
-//        private static void ImprimirCursosDoWhile(Curso[] arregloCursos)
-//        {
-//            int contador = 0;
-//            do
-//            {
-//                Console.WriteLine($"Nombre {arregloCursos[contador].Name}, Id  {arregloCursos[contador].UniqueId}");
-//                contador++;
-//            } while (contador < arregloCursos.Length);
-//        }
-
-//        private static void ImprimirCursosFor(Curso[] arregloCursos)
-//        {
-//            for (int i = 0; i < arregloCursos.Length; i++)
-//            {
-//                Console.WriteLine($"Nombre {arregloCursos[i].Name}, Id  {arregloCursos[i].UniqueId}");
-//            }
-//        }
-
-//        private static void ImprimirCursosForEach(Curso[] arregloCursos)
-//        {
-//            foreach (var curso in arregloCursos)
-//            {
-//                Console.WriteLine($"Nombre {curso.Name}, Id  {curso.UniqueId}");
-//            }
-//        }
-//    }
-//}
